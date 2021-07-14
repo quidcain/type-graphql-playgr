370dd284c69204e360eb310792e1d651c8e7e049
Daniel Radezky <danielradezky@gmail.com>
2021-07-14 11:35:07 +0300

    Execute schemas without directive declaration

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,17 @@ import { ApolloServer } from "apollo-server";
 import { FriendsResolver } from "./friends/resolver";
 import { buildSchema } from "type-graphql";
 import { stringifySchema } from "./printSchema";
+import { SchemaDirectiveVisitor } from "graphql-tools";
+import { AuthorizedDirective } from "./authorizedDirective";
 
 async function bootstrap() {
   // build TypeGraphQL executable schema
   const schema = await buildSchema({
     resolvers: [FriendsResolver],
+  });
+
+  SchemaDirectiveVisitor.visitSchemaDirectives(schema, {
+    authorized: AuthorizedDirective,
   });
 
   const schemaString = stringifySchema(schema);
